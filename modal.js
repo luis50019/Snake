454c91d236742d327b9145ccfc7305edad4343f7
luis50019 <teddiazdiaz019@gmail.com>
2024-08-23 14:08:23 -0600

    corrigiendo bugs

--- modal.js
+++ modal.js
@@ -1,8 +1,8 @@
 const btnPlay = document.getElementById('btn-play');
 const modalScore = document.getElementById('modal-score');
+const modal = document.getElementById('modal');
 
 function showModal() {
-  const modal = document.getElementById('modal');
   if (modal.classList.length == 1) {
     modalScore.innerHTML = 'Score ' + localStorage.getItem('score')
   } else {
@@ -12,10 +12,8 @@ function showModal() {
 showModal();
 
 btnPlay.addEventListener('click', () => {
-  setTimeout(() => {
-    modal.classList.add('not-active');
-    // Verificar si realmente deseas recargar la página
-    window.localStorage.setItem('score',0)
-    window.location.reload()
-  }, 200);
+  modal.classList.add('not-active');
+  // Verificar si realmente deseas recargar la página
+  window.localStorage.setItem('score', 0)
+  window.location.reload()
 });
