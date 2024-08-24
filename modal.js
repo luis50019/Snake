be2af78f96f7aae5cb5217b7e9d4b4519f7ebcb6
luis50019 <teddiazdiaz019@gmail.com>
2024-08-23 18:54:41 -0600

    bug

--- modal.js
+++ modal.js
@@ -13,12 +13,14 @@ showModal();
 
 btnPlay.addEventListener('click', () => {
   modal.classList.add('not-active');
+  alert('click')
   // Verificar si realmente deseas recargar la página
   window.localStorage.setItem('score', 0)
   // window.location.reload()
 });
 btnPlay.addEventListener('touchstart', () => {
   modal.classList.add('not-active');
+  alert('click')
   // Verificar si realmente deseas recargar la página
   window.localStorage.setItem('score', 0)
   // window.location.reload()
