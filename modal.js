40a0ee5a0325a3d7862179b7e6def6e3f711ba4d
luis50019 <teddiazdiaz019@gmail.com>
2024-08-23 18:52:33 -0600

    agregado touchstart

--- modal.js
+++ modal.js
@@ -17,3 +17,9 @@ btnPlay.addEventListener('click', () => {
   window.localStorage.setItem('score', 0)
   // window.location.reload()
 });
+btnPlay.addEventListener('touchstart', () => {
+  modal.classList.add('not-active');
+  // Verificar si realmente deseas recargar la página
+  window.localStorage.setItem('score', 0)
+  // window.location.reload()
+});
